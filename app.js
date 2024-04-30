4924b74a486939f655dddc765a763481b30ba05c
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-04-30 22:02:57 +0800

    建立Schema及新增資料

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 app.set("view engine", "ejs");
 
@@ -14,6 +15,36 @@ mongoose
   });
 // why是27017，在cmd輸入指令mongosh即可知道
 // 要連結哪個database？在cmd輸入指令show dbs即可知道目前有哪些database
+
+const studentSchema = new Schema({
+  name: String,
+  age: Number,
+  major: String,
+  scholarship: {
+    merit: Number,
+    other: Number,
+  },
+});
+
+const Student = mongoose.model("Student", studentSchema);
+const newObject = new Student({
+  name: "Esther",
+  age: 27,
+  major: "Mathematics",
+  scholarship: {
+    merit: 6000,
+    other: 7000,
+  },
+});
+newObject
+  .save()
+  .then((saveObject) => {
+    console.log("資料已經儲存完畢，儲存的資料是：");
+    console.log(saveObject);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.listen(3000, () => {
   console.log("伺服器正在聆聽port 3000...");
