bbf9ffccd110c0e7ab05171e53801f50d3db0ad7
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-05-03 22:43:29 +0800

    middleware 釋例

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const fs = require("fs");
 
 app.set("view engine", "ejs");
 
@@ -15,75 +16,6 @@ mongoose
   });
 // why是27017，在cmd輸入指令mongosh即可知道
 // 要連結哪個database？在cmd輸入指令show dbs即可知道目前有哪些database
-
-// 法1
-const studentSchema = new Schema(
-  {
-    name: { type: String, required: true, maxlength: 25 },
-    age: { type: Number, min: [0, "年齡不能小於0"] },
-    // major: { type: String, required: [true, "每位學生都需要選至少一個主修"] },
-    major: {
-      type: String,
-      required: function () {
-        return this.scholarship.merit >= 3000;
-      },
-      // 若 merit >= 3000，則 major就是必填；若 merit < 3000，則不是必填
-      enum: [
-        "Chemistry",
-        "Computer Science",
-        "Mathematics",
-        "Civil Engineering",
-        "undecided",
-      ],
-      // 若所填的值，都不符合enum裡面的內容，就會出現問題
-      // 這功能適合用在會員分級
-    },
-
-    scholarship: {
-      merit: { type: Number, default: 0 },
-      other: { type: Number, default: 0 },
-    },
-  },
-  {
-    statics: {
-      findAllMajorStudents(major) {
-        return this.find({ major: major }).exec();
-      },
-    },
-  }
-);
-
-// 法2
-const studentSchema = new Schema({
-  name: { type: String, required: true, maxlength: 25 },
-  age: { type: Number, min: [0, "年齡不能小於0"] },
-  // major: { type: String, required: [true, "每位學生都需要選至少一個主修"] },
-  major: {
-    type: String,
-    required: function () {
-      return this.scholarship.merit >= 3000;
-    },
-    // 若 merit >= 3000，則 major就是必填；若 merit < 3000，則不是必填
-    enum: [
-      "Chemistry",
-      "Computer Science",
-      "Mathematics",
-      "Civil Engineering",
-      "undecided",
-    ],
-    // 若所填的值，都不符合enum裡面的內容，就會出現問題
-    // 這功能適合用在會員分級
-  },
-
-  scholarship: {
-    merit: { type: Number, default: 0 },
-    other: { type: Number, default: 0 },
-  },
-});
-
-studentSchema.statics.findAllMajorStudents = function (major) {
-  return this.find({ major: major }).exec();
-};
 
 // 法3
 const studentSchema = new Schema({
@@ -117,11 +49,32 @@ studentSchema.static("findAllMajorStudents", function (major) {
   return this.find({ major: major }).exec();
 });
 
+studentSchema.methods.printTotalScholarship = function () {
+  return this.scholarship.merit + this.scholarship.other;
+};
+
+studentSchema.pre("save", () => {
+  fs.writeFile("record.txt", "A new data will be saved...", (e) => {
+    if (e) throw e;
+  });
+});
+
 const Student = mongoose.model("Student", studentSchema);
 
-Student.findAllMajorStudents("CS")
+let newStudent = new Student({
+  name: "小明",
+  age: 30,
+  major: "Computer Science",
+  scholarship: {
+    merit: 5000,
+    other: 1000,
+  },
+});
+
+newStudent
+  .save()
   .then((data) => {
-    console.log(data);
+    console.log("資料已經儲存");
   })
   .catch((e) => {
     console.log(e);
