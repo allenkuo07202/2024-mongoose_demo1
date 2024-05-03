27122d44664cb02596e6163865e856fb6e3e21ea
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-05-03 14:40:21 +0800

    instance method 釋例

--- app.js
+++ app.js
@@ -16,6 +16,45 @@ mongoose
 // why是27017，在cmd輸入指令mongosh即可知道
 // 要連結哪個database？在cmd輸入指令show dbs即可知道目前有哪些database
 
+// 法1
+const studentSchema = new Schema(
+  {
+    name: { type: String, required: true, maxlength: 25 },
+    age: { type: Number, min: [0, "年齡不能小於0"] },
+    // major: { type: String, required: [true, "每位學生都需要選至少一個主修"] },
+    major: {
+      type: String,
+      required: function () {
+        return this.scholarship.merit >= 3000;
+      },
+      // 若 merit >= 3000，則 major就是必填；若 merit < 3000，則不是必填
+      enum: [
+        "Chemistry",
+        "Computer Science",
+        "Mathematics",
+        "Civil Engineering",
+        "undecided",
+      ],
+      // 若所填的值，都不符合enum裡面的內容，就會出現問題
+      // 這功能適合用在會員分級
+    },
+
+    scholarship: {
+      merit: { type: Number, default: 0 },
+      other: { type: Number, default: 0 },
+    },
+  },
+  {
+    methods: {
+      printTotalScholarship() {
+        return this.scholarship.merit + this.scholarship.other;
+      },
+    },
+  }
+);
+// 之前的schema setting為1st參數，methods物件為2nd參數
+
+// 法2
 const studentSchema = new Schema({
   name: { type: String, required: true, maxlength: 25 },
   age: { type: Number, min: [0, "年齡不能小於0"] },
@@ -43,38 +82,20 @@ const studentSchema = new Schema({
   },
 });
 
+studentSchema.methods.printTotalScholarship = function () {
+  return this.scholarship.merit + this.scholarship.other;
+};
+
 const Student = mongoose.model("Student", studentSchema);
 
-// required示範(若 merit >= 3000，則 major就是必填)
-// let newStudent = new Student({
-//   name: "Wilson",
-//   age: 27,
-//   scholarship: { merit: 1500, other: 2000 },
-// });
-
-// enum示範(major輸入enum以外的值)
-// let newStudent = new Student({
-//   name: "Jared",
-//   age: 27,
-//   major: "Nuclear Engineering",
-//   scholarship: { merit: 1500, other: 2000 },
-// });
-
-// maxlength示範(name長度不能超過25)
-let newStudent = new Student({
-  name: "ahfladhfaliufhluifhuulsifhasilfhi",
-  age: 27,
-  major: "Computer Science",
-  scholarship: { merit: 1500, other: 2000 },
-});
-
-newStudent
-  .save()
-  .then((data) => {
-    console.log("資料儲存成功");
-  })
-  .catch((e) => {
-    console.log(e);
+Student.find({})
+  .exec()
+  .then((arr) => {
+    arr.forEach((student) => {
+      console.log(
+        student.name + "的總獎學金金額是" + student.printTotalScholarship()
+      );
+    });
   });
 
 app.listen(3000, () => {
